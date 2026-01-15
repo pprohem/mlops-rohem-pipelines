2db87f8bcfd154cc4df045b250d7dd7cfd81da25
Pedro Rohem <110731287+pprohem@users.noreply.github.com>
2026-01-15 13:27:48 -0300

    Remove unused batch prediction and retraining pipelines from module exports

--- src/mlops_pipelines/pipelines/__init__.py
+++ src/mlops_pipelines/pipelines/__init__.py
@@ -1,9 +1,6 @@
 from .training import training_pipeline
-from .batch_prediction import batch_prediction_pipeline
-from .retraining import retraining_pipeline
+
 
 __all__ = [
-    "training_pipeline",
-    "batch_prediction_pipeline",
-    "retraining_pipeline",
+    "training_pipeline"
 ]
